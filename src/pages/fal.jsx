aff19b6d51b4bd2eef20f89ee0c2b793335edf58
Fateme Kholousi <f.kholousi@authin.ir>
2022-01-01 21:31:00 +0330

    feat : poem page created but there's a problem with htmlText from API

--- src/pages/fal.jsx
+++ src/pages/fal.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+//stackoverflow.com/questions/19266197/reactjs-convert-html-string-to-jsx
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./fal.css";
 
-function fal() {
-  return <div>blue</div>;
+function Faal() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const response = axios
+      .get("https://ganjgah.ir/api/ganjoor/hafez/faal")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+
+    // const indexOfchoosenFaal = Math.floor(Math.random() * data.length);
+    // const choosenFaal = data;
+  }, []);
+  console.log(data.title);
+  const htmlText = data.htmlText;
+  //   console.log(typeof data);
+
+  return (
+    <div className="fal--container">
+      <h1>{data.title}</h1>
+      {htmlText}
+    </div>
+  );
 }
 
-export default fal;
+export default Faal;
